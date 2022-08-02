1f378de4cbdd2767fca060c08a559eb449ddedb9
tom <thomarhughes98@gmail.com>
2022-08-02 09:43:12 +0100

    this is human readable and the start of styling my page

--- index.php
+++ index.php
@@ -11,15 +11,26 @@ $query = $db->prepare($queryString);
 $query->execute();
 $allResults = $query->fetchAll();
 
-
 ?>
 
 <!DOCTYPE html>
 <html>
-<head></head>
+<head>
+    <link rel="stylesheet" type="text/css' href="normalize.css">
+    <link rel="stylesheet" type="text/css' href="style.css">
+</head>
 <body>
     <h1>ENDANGERED ANIMAL LIST</h1>
-    
+    <div>
+    <ul>
+        <?php $animals = $allResults;
+        foreach($animals as $animal) { ?>
+            <li><?php echo $animal['species']; ?></li>
+            <li><?php echo $animal['population']; ?></li>
+            <li><?php echo $animal['country']; ?></li>
+        <?php } ?>
+    </ul>
+    </div>
 </body>
 
 </html>
